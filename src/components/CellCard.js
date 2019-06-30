ffa4dd0d59a19b820c5efdfcd71ad9d3ec2db52c
Leslie <rleslie1015@gmail.com>
2019-06-30 01:03:33 -0500

    awww yeah now were talking- image works

--- src/components/CellCard.js
+++ src/components/CellCard.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle, Button } from 'reactstrap';
-import Cellphone from './Cellphone.jpeg';
+import images from './images.png';
 
 const CellCard = (props) => {
   return (
     <div>
       <Card>
-        <CardImg top width="100%" src={Cellphone} alt="Card image cap" />
+        <CardImg top width="100%" src={images} alt="Card image cap" />
         <CardBody>
           <CardTitle>Card title</CardTitle>
           <CardSubtitle>Card subtitle</CardSubtitle>
