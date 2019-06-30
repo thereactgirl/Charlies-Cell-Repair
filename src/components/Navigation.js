e314821c94f0715437cf64a256ebda108cde3817
Leslie <rleslie1015@gmail.com>
2019-06-30 00:08:30 -0500

    practicing react-strap

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { Container, Button, Alert } from 'reactstrap';
 
-function Navigation(){
-    return (
-        <h1>CPR</h1>
-    )
+class Navigation extends React.Component{
+    state = {
+        visible: true
+    }
+
+    toggle() {
+        this.setState({
+            visible: !this.state.visible
+        })
+    }
+    render(){
+        return (
+            <div>
+                <h1>CPR</h1>
+                <nav>
+                    <Button color="secondary" active={true}>Home</Button>
+                    <Button color="secondary" active={true}>About</Button>
+                    <Button color="secondary" active={true}>Services</Button>
+                    <Button color="secondary" active={true}>Contact</Button>
+                    <Alert color="primary" isOpen={this.state.visible} toggle={this.toggle.bind(this)}> Hi! I am am alert.</Alert>
+                </nav>
+            </div>
+        )
+    }
 } 
 
 export default Navigation;
