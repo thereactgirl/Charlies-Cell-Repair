fa35d235321e78a0c0912f2e3b4eb209b96e4a65
Leslie <rleslie1015@gmail.com>
2019-06-30 00:12:52 -0500

    Alert from ReactStrap fun stuff

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,7 @@ import { Container, Button, Alert } from 'reactstrap';
 
 class Navigation extends React.Component{
     state = {
-        visible: true
+        visible: false
     }
 
     toggle() {
@@ -17,10 +17,10 @@ class Navigation extends React.Component{
                 <h1>CPR</h1>
                 <nav>
                     <Button color="secondary" active={true}>Home</Button>
-                    <Button color="secondary" active={true}>About</Button>
-                    <Button color="secondary" active={true}>Services</Button>
-                    <Button color="secondary" active={true}>Contact</Button>
-                    <Alert color="primary" isOpen={this.state.visible} toggle={this.toggle.bind(this)}> Hi! I am am alert.</Alert>
+                    <Button color="secondary" active={true} onClick={this.toggle.bind(this)}>About</Button>
+                    <Button color="secondary" active={true} onClick={this.toggle.bind(this)}>Services</Button>
+                    <Button color="secondary" active={true} onClick={this.toggle.bind(this)}>Contact</Button>
+                    <Alert color="primary" isOpen={this.state.visible} toggle={this.toggle.bind(this)}> Sorry! That page is still being built.</Alert>
                 </nav>
             </div>
         )
